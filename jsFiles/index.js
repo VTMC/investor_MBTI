0c774ba7ed667ac8ede8d8346295ab305a07e7cb
VTMC_Enchant <34031136+VTMC@users.noreply.github.com>
2026-05-26 02:55:26 +0900

    [investor_MBTI] set selector
    1. 구글 스프레드시트에서 "SOLVER_Scenarios"에 해당하는 시나리오를 Selector에 불러와서 표시하도록 기능 추가
    2. Scenario Selector에서 시나리오를 선택할 경우, 그에 해당하는 R,S,C,H값을 적용.
    3. 각 시나리오를 선택하면 그에 맞는 값을 R,S,C,H 값과 설명글에 적용하도록 적용.
    4. custom 시나리오 이외에는 값 변경 불가, custom 에서는 원하는 값을 넣을 수 있음.

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -1,4 +1,13 @@
-const SCRIPT_URL = "https://docs.google.com/spreadsheets/d/1qHDKKrVmFucePpcuPVDWSo4g0vKudzUIgb08Kyn8LpY/edit?usp=sharing";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyauDmWhToMpGQQPQQIXVGVQubbyZH3Xut4G8Y8C7gKULLOCl4o3bNxvGqWwU1zD6eqhA/exec";
+
+var scenarioData;
+
+var selectElement = document.querySelector("select[name='option1']");
+var rInputElement = document.getElementById("r_input");
+var sInputElement = document.getElementById("s_input");
+var cInputElement = document.getElementById("c_input");
+var hInputElement = document.getElementById("h_input");
+var scenarioDescriptor = document.querySelector(".description_of_scenario");
 
 function doGet() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
@@ -17,8 +26,85 @@ async function fetchData() {
   }
 }
 
-function main(){
-    fetchData();
+async function getScenario() {
+  try{
+    const response = await fetch(SCRIPT_URL+"?action=getScenario");
+    scenarioData = await response.json();
+  
+    console.log("Scenario Data:", scenarioData); // Check Scenario Data
+
+    scenarioData.data.forEach(row => {
+      const optionValue = row[0];
+      if(optionValue != null && optionValue.trim() !== ""){
+        const option = document.createElement("option");
+        option.value = optionValue;
+        option.textContent = optionValue;
+        selectElement.appendChild(option);
+      }
+    });
+
+    const customOption = Array.from(selectElement.options).find(option => {
+      return option.value.toLowerCase() === "custom";
+    });
+
+    if (customOption) {
+      selectElement.value = customOption.value;
+    } else {
+      // custom이 없으면 마지막 option 선택
+      selectElement.selectedIndex = selectElement.options.length - 1;
+    }
+
+    selectElement.addEventListener("change", async function() {
+      const selectedScenario = this.value;
+
+      console.log("Selected Scenario : ", selectedScenario);
+
+      if(!selectedScenario){
+        scenarioDescriptor.textContent = "wrong scenario selected!";
+        scenarioDescriptor.style.color = "red";
+        scenarioDescriptor.style.fontWeight = "bold";
+        return;
+      }
+
+      const detailDataOfScenario = await getScenarioDetail(selectedScenario);
+
+      console.log("Detail Data of Scenario : ", detailDataOfScenario);
+
+      rInputElement.value = detailDataOfScenario.data.r_value;
+      sInputElement.value = detailDataOfScenario.data.s_value;
+      cInputElement.value = detailDataOfScenario.data.c_value;
+      hInputElement.value = detailDataOfScenario.data.h_value;
+      scenarioDescriptor.textContent = detailDataOfScenario.data.note;
+
+      if(selectedScenario === "custom"){
+        rInputElement.readOnly = false;
+        sInputElement.readOnly = false;
+        cInputElement.readOnly = false;
+        hInputElement.readOnly = false;
+      }
+
+    });
+
+    selectElement.dispatchEvent(new Event("change"));
+  }catch(error){
+    console.error("Error to load Scenario :", error);
+  }
+}
+
+async function getScenarioDetail(scenarioName){
+  try{
+    const response = await fetch(SCRIPT_URL+"?action=getScenarioDetail&scenarioName="+encodeURIComponent(scenarioName));
+    const data = await response.json();
+    return data;
+  }catch(error){
+    console.error("Error to load Scenario Detail :", error);
+    return null;
+  }
+}
+
+async function main(){
+  await getScenario();
+  // await fetchData();
 }
 
 main();
